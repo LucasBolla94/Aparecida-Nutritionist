17ba5408aec90b972a837b75fc8ce97fd520e735
Lucas Bolla <lucasbolla@icloud.com>
2022-10-18 06:19:52 +0100

    Projeto Completo

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var tittle = document.querySelector('.tittle');
+tittle.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for(var i = 0; i < pacientes.length ; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector('.info-imc');
+
+    var validaPeso = true;
+    var validaAltura = true;
+
+    if (peso <= 20 || peso >= 400) {
+    console.log("Peso Invalido");
+    validaPeso = false;
+    tdPeso.textContent = "Peso Invalido";
+    paciente.style.backgroundColor = "lightcoral";
+    paciente.style.fontWeight = 600;
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura Invalida");
+        validaAltura = false;
+        tdAltura.textContent = "Altura Invalida";
+    }
+
+    if ( validaPeso && validaAltura) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+        tdImc.style.backgroundColor = 'lightgreen';
+        tdImc.style.border = '1px black solid';
+        tdImc.style.fontWeight = 600;
+    }
+};
+
+function montaTD(dado, classe) {
+	var td = document.createElement("td");
+	td.textContent = dado;
+	td.classList.add(classe);
+};

9725f3279da253bff0dc025d6830715dd4958a5e
Lucas Bolla <lucasbolla@icloud.com>
2022-10-22 04:37:19 +0100

    API Connection- Checking response API

--- js/downloadpatient.js
+++ js/downloadpatient.js
@@ -6,13 +6,19 @@ download.addEventListener("click", function(){
     conn.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     conn.addEventListener("load", function(){
-        var response = conn.responseText;
-        var patients = JSON.parse(response);
-        patients.forEach( function(patient){
-            var patientTr = createTr(patient);
-            var tabela = document.querySelector("#tabela-pacientes");
-            tabela.appendChild(patientTr)
-        });
+        if ( conn.status == 200){
+            var response = conn.responseText;
+            var patients = JSON.parse(response);
+            patients.forEach( function(patient){
+                var patientTr = createTr(patient);
+                var tabela = document.querySelector("#tabela-pacientes");
+                tabela.appendChild(patientTr)
+            });
+        }else{
+            var er404 = document.querySelector("#er404");
+            er404.classList.remove("pesq1");
+            er404.textContent = conn.responseText;
+        }
     });
 
     conn.send();
